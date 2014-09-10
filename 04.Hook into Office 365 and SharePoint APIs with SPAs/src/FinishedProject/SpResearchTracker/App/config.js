a156a3fe8af83ceccc4c0e74ba25d6906d26ada7
Andrew Connell <me@andrewconnell.com>
2014-09-10 16:05:39 -0400

    added finished product

---
+++ 04.Hook into Office 365 and SharePoint APIs with SPAs/src/FinishedProject/SpResearchTracker/App/config.js
@@ -0,0 +1,36 @@
+﻿/*
+ * role: global app configuration
+ * desc: used for global app configuration
+ */
+(function () {
+  'use strict';
+
+  // get a reference to the app module
+  var app = angular.module('app');
+
+  // collection of events used throughout the app
+  var events = {
+    // event when the controller has been successfully activated
+    controllerActivateSuccess: 'controller.activateSuccess',
+  };
+
+  // create global static config object
+  var config = {
+    title: 'Project Research Tracker App',
+    events: events
+  };
+  app.value('config', config);
+
+  // setup the angular logging provider to debug=on
+  app.config(['$logProvider', function($logProvider) {
+    if ($logProvider.debugEnabled) {
+      $logProvider.debugEnabled(true);
+    }
+  }]);
+
+  // configure the common configuration
+  app.config(['commonConfigProvider', function (cfg) {
+    // setup app events
+    cfg.config.controllerActivateSuccessEvent = config.events.controllerActivateSuccess;
+  }]);
+})();
