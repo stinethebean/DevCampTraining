0710ddc45f4399ec258beb24fc5191d31d22c3e7
Benjamin Fox <benjaminf@intergen.co.nz>
2015-03-11 12:00:09 +1300

    Removed test configuration

--- 06.Hook into SharePoint APIs with Android/src/o365-tasks/app/src/main/java/com/microsoft/o365_tasks/Constants.java
+++ 06.Hook into SharePoint APIs with Android/src/o365-tasks/app/src/main/java/com/microsoft/o365_tasks/Constants.java
@@ -2,13 +2,13 @@ package com.microsoft.o365_tasks;
 
 public class Constants {
 
-    public static final String AAD_DOMAIN = "o365performancerecorder.onmicrosoft.com";
-    public static final String AAD_CLIENT_ID = "566c7295-9bdf-4d83-81e6-92db6d7ae4b3";
+    public static final String AAD_DOMAIN = "YOURDOMAIN.onmicrosoft.com";
+    public static final String AAD_CLIENT_ID = "00000000-0000-0000-0000-000000000000";
     public static final String AAD_LOGIN_HINT = "admin@" + AAD_DOMAIN;
     public static final String AAD_AUTHORITY = "https://login.windows.net/" + AAD_DOMAIN;
     public static final String AAD_REDIRECT_URL = "http://android/complete";
     
-    public static final String SHAREPOINT_URL = "https://o365performancerecorder.sharepoint.com";
+    public static final String SHAREPOINT_URL = "https://YOURDOMAIN.sharepoint.com";
     public static final String SHAREPOINT_SITE_PATH = "";
     public static final String SHAREPOINT_LIST_NAME = "Tasks for Android";
 
